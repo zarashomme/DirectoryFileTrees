efee4414b09d4992ecad0c81eb04e8cdf1620d93
AryahP <108384070+AryahP@users.noreply.github.com>
2025-11-07 09:28:13 -0500

    Update bdt_client.c

--- 1BDT/bdt_client.c
+++ 1BDT/bdt_client.c
@@ -120,7 +120,7 @@ int main(void) {
   assert(BDT_rm("1root") == NO_SUCH_PATH);
   assert(BDT_rm("1anotherroot") == NO_SUCH_PATH);
   /*assert((temp = BDT_toString()) != NULL);*/
-  assert(!strcmp(temp,""));
+  a*ssert(!strcmp(temp,""));*/
   /*free(temp);*/
 
   /* removing a first child should cause the second child to become
